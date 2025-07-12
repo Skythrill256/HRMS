c2f23f4c784d3130666009e8d15237728165dc22
Utpal Barman <barmanutpal022@gmail.com>
2025-07-12 10:39:19 +0530

    render issue solved

--- Frontend/src/Dashboards/Admin-Dashboard/projects/TotalProjects.jsx
+++ Frontend/src/Dashboards/Admin-Dashboard/projects/TotalProjects.jsx
@@ -1,12 +1,13 @@
 import { useNavigate, useParams, Routes, Route,  } from "react-router-dom";
 import Workordergeneratedproject from "./workordergeneratedproject";
 import ProjectProfile from "./ProjectProfile";
+import AddProject from "./AddProject";
 const TotalProjects = () => {
   return(
     <Routes>
       <Route path="/" element={<Workordergeneratedproject/>}/>
       <Route path="profile/:projectId" element={<ProjectProfile/>}/>
-      
+      <Route path="addproject/:id" element={<AddProject />} />     
     </Routes>
   )
 }
